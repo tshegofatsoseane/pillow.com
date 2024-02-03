69f751622eb073203748256346c7433f88f9d672
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-02-03 21:00:31 +0200

    first commit

---
+++ accommodations/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Accommodation(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    accreditation_number = models.CharField(max_length=255)
+    number_of_beds = models.IntegerField()
+    email = models.EmailField(max_length=255)
+    cell_number = models.CharField(max_length=20)
+    street_address = models.CharField(max_length=255)
+    nearest_campus = models.CharField(max_length=255)
+    residence_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.residence_name
+
+    class Meta:
+        db_table = 'Accommodations'
---
+++ accommodations/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from .models import Accommodation
+
+def index(request):
+    # Get the search query from the URL parameters
+    query = request.GET.get('q')
+    
+    # Fetch all accommodations from the database
+    accommodations_list = Accommodation.objects.all()
+
+    # If there's a search query, filter the accommodations list
+    if query:
+        accommodations_list = accommodations_list.filter(
+            Q(residence_name__icontains=query) |
+            Q(street_address__icontains=query) |
+            Q(email__icontains=query) |
+            Q(nearest_campus__icontains=query)
+        )
+
+    # Paginate the filtered accommodations list, showing 10 accommodations per page
+    paginator = Paginator(accommodations_list, 3)
+    
+    # Get the page number from the URL parameters
+    page_number = request.GET.get('page')
+    
+    try:
+        # Try to fetch the requested page of accommodations
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If the page parameter is not an integer, show the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # If the page parameter is out of range, show the last page
+        page_obj = paginator.page(paginator.num_pages)
+
+    # Pass the paginated accommodations to the template for rendering
+    return render(request, 'accommodations/index.html', {'page_obj': page_obj, 'query': query})
